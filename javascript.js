67c630383a6c22b7a964b61c577b8b6919093404
Jason Scott <jcpoems@gmail.com>
2024-07-22 01:12:00 -0400

    Skeleton Files

---
+++ javascript.js
@@ -0,0 +1,72 @@
+console.log("Hey J");
+
+const gameBoard = (function() {
+    //private
+    const board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+
+    // ...board provides a shallow copy of board so im not letting user access board
+    function getBoard() {return [...board];}
+    function makeMove(index, player) {board[index] = player;}
+    
+    //public API
+    return{
+        getBoard: getBoard,
+        makeMove: makeMove
+    };
+})();
+
+const game = (function() {
+
+    const winConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]             // Diagonals
+    ];
+
+    function checkWin(moves) {
+        for(let i = 0; i < winConditions.length; i++){
+            if(winConditions[i].every(move => moves.includes(move))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function checkDraw(board) { return !board.includes(' ');}
+
+    return{
+        checkWin: checkWin,
+        checkDraw: checkDraw
+    };
+})();
+
+
+function Player(name,symbol){
+    this.name = name;
+    this.symbol = symbol;
+    this.moves = [];
+}
+
+const p1 = new Player("Jason", 'X');
+const p2 = new Player("Adam", 'O');
+
+// other stuff
+//garbage
+
+
+
+p2.moves.push(0);
+p2.moves.push(1);
+p2.moves.push(1);
+
+console.log("FRED");
+console.log(game.checkWin(p2.moves));
+
+
+console.table(gameBoard.getBoard());
+gameBoard.makeMove(1, 'x');
+
+
+console.log(gameBoard.getBoard().includes('o'));
+
+console.log(game.checkWin([0,1,3,2]));
